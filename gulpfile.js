a26a86cbc152908b5f2bf01225d0df768b3f1b6b
Cora <cora@yunio.com>
2019-08-07 17:51:34 +0800

    gulp修改

--- gulpfile.js
+++ gulpfile.js
@@ -39,12 +39,12 @@ function tasksass (){
 	.pipe(connect.reload());
 }
 
-gulp.task('html',function(cb){
+function html(){
 	return gulp.src('src/html/*.html')
-	// .pipe(browserSync.reload({ stream: true }))//这个暂时没有找到好的实现方法
+	// .pipe(browserSync.reload({ stream: true }))//这个暂时没有找到好的实现方法 看似好像没有用
 	.pipe(gulp.dest('dist/html'))
 	.pipe(connect.reload());
-})
+}
 //实现浏览器同步更新 浏览器输入http://localhost:8080 
 gulp.task('connect',function(){
 	connect.server({
@@ -53,14 +53,16 @@ gulp.task('connect',function(){
 			port: 8080//端口
 	})
 });
-gulp.task('sass', tasksass);
+// gulp.task('sass', tasksass);
+// gulp.task('html', html);
 //parallel(...tasks)：多个任务同时执行 series(...tasks)：顺序执行多个任务
-exports.sass = gulp.parallel(tasksass);
-exports.build = gulp.parallel(tasksass);
+// exports.sass = gulp.parallel(tasksass);
+// exports.build = gulp.parallel(tasksass);
 
 gulp.task('watchs',function(){
-	gulp.watch('src/html/*.html',gulp.series('html'));
+	gulp.watch('src/html/*.html',gulp.parallel(html));
 	gulp.watch('src/style/*.scss', gulp.parallel(tasksass));
 })
 
-gulp.task('default',gulp.series(gulp.parallel('connect','watchs',tasksass,'html')));;
+// gulp.task('default',gulp.series(gulp.parallel('connect','watchs',tasksass,html)));
+exports.default = gulp.series(gulp.parallel('connect','watchs',tasksass,html));
